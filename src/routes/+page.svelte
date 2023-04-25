8d5db84d299bf26cc5505b8a2d3984941608d2e9
Rahardiyan E. Widiatmoko <rahardiyanekowidiatmoko@gmail.com>
2023-04-25 14:14:20 +0700

    Adding styles & fonts

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,11 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<div class="flex flex-col h-screen justify-center items-center">
+  <img
+    src="https://media.giphy.com/media/3ohuP86bdGnXtyqtDG/giphy.gif"
+    alt="under construction"
+    class="h-64 rounded-xl"
+  />
+  <h1 class="text-app_black dark:text-app_white text-3xl font-bold mt-8">
+    We're still building! 🏗️
+  </h1>
+  <p class="text-gray-400 mt-2">Traxpired is still under construction</p>
+</div>
---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./src/**/*.{html,js,svelte,ts}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ["Jost", "sans-serif"],
+      },
+      colors: {
+        app_black: "#211D1E",
+        app_white: "#F5F8FA",
+        app_gray: "#909FB7",
+        app_light: "#BFC8D9",
+        app_dark: "#54585F",
+        app_primary_dark: "#24BC9E",
+        app_primary_light: "#50CEB4",
+        app_error: "#EE2B2B",
+        app_background: "#2B2627",
+      },
+    },
+  },
+  plugins: [],
+};
